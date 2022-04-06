f2ea7e3a58f57b9c48733effccdfcbe1ce88e1be
Namrata <Namrata@DESKTOP-30P8MF5.mshome.net>
2022-04-07 00:49:45 +0530

    second commit

---
+++ ShopModule1/src/test/java/com/tns/shop/ShopModule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopModule1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
